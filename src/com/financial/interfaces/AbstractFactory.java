9dbbad681ce8f71b8a786a09736417e7126a1f6e
Keshav <k.keshav9984@gmail.com>
2014-11-16 10:59:34 -0600

    Added AbsteactFactory

---
+++ src/com/financial/interfaces/AbstractFactory.java
@@ -0,0 +1,17 @@
+package com.financial.interfaces;
+
+public class AbstractFactory {
+
+	public ITransaction createTransaction() {
+		return null;
+	}
+	
+	public ICustomer createCustomer(){
+		return null;
+	}
+	
+	public IAccount createAccount(){
+		return null;
+	}
+
+}

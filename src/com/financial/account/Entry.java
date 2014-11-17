71550ede210013a5db77a02ef6509c3b23e6a76f
rachana <rachana@rachana-XPS-L412Z>
2014-11-17 14:43:23 -0600

    Account added

--- src/com/financial/account/Entry.java
+++ src/com/financial/account/Entry.java
@@ -4,7 +4,37 @@ import java.util.Date;
 
 public class Entry {
 	
-	double amount;
-	Date entryDate;
-	String entryInfo;
+	private double amount;
+	private Date entryDate;
+	private String entryInfo;
+	
+	public Entry(double amount, Date entryDate, String entryInfo) {
+		this.amount    = amount;
+		this.entryDate = entryDate;
+		this.entryInfo = entryInfo;
+	}
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Date getEntryDate() {
+		return entryDate;
+	}
+
+	public void setEntryDate(Date entryDate) {
+		this.entryDate = entryDate;
+	}
+
+	public String getEntryInfo() {
+		return entryInfo;
+	}
+
+	public void setEntryInfo(String entryInfo) {
+		this.entryInfo = entryInfo;
+	}
+
 }
--- src/com/financial/interfaces/IAccount.java
+++ src/com/financial/interfaces/IAccount.java
@@ -1,11 +1,22 @@
 package com.financial.interfaces;
 
+import java.util.List;
+
 import com.financial.account.Entry;
 
 public interface IAccount {
-	double getCurrentBalance();
-	void deposite();
-	void withdraw();
-	void addEntry(Entry entry);
-	void notifyCustormers();
+
+	public boolean deposite(double amount);
+	public double getBalance();
+	public boolean withdraw(double amount);
+	public void addEntry(Entry entry);
+	public boolean sendEmail();
+	public boolean hasToSendMail();
+	
+	public String getAccountNumber();
+	public void setAccountNumber(String accountNumber);
+	public List<Entry> getEntryList();
+	public void setEntryList(List<Entry> entryList);
+	public double getTotalBalance();
+	public void setTotalBalance(double totalBalance);
 }

d5428a3bdc44fb8e509c2ba5e5ef3a98e32f4b77
rachana <rachana@rachana-XPS-L412Z>
2014-11-18 17:14:25 -0600

    Minor changes

--- src/com/financial/factories/SingletonFactory.java
+++ src/com/financial/factories/SingletonFactory.java
@@ -1,9 +1,15 @@
 package com.financial.factories;
 
+import com.financial.account.AccountManager;
 import com.financial.interfaces.ITransactionManager;
+import com.financial.transaction.TransactionManager;
 
 public class SingletonFactory {
 	public static ITransactionManager getTransactionManager(){
-		return null;
+		return TransactionManager.transactionManager;
+	}
+	
+	public static AccountManager getAccountManager(){
+		return AccountManager.accountManager;
 	}
 }

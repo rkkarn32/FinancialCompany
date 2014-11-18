6d5ce70db46cddd1aa8915dbec831651eb7bbb1c
rkkarn32 <rk.karn32@gmail.com>
2014-11-17 21:39:30 -0600

    Abstract Form completed

--- src/com/financial/view/AbstractFrm.java
+++ src/com/financial/view/AbstractFrm.java
@@ -6,14 +6,21 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-public class AbstractFrm extends JFrame{
+import sun.font.EAttribute;
+
+public abstract class AbstractFrm extends JFrame{
 	
 	/**
 	 *Serial version  
 	 */
 	
 	private static final long serialVersionUID = 1L;
+	
+	private DefaultTableModel model= new DefaultTableModel();
 
 	/*
 	 * Constructor for set a default view
@@ -23,19 +30,22 @@ public class AbstractFrm extends JFrame{
 		setBounds(10, 10, 300, 300);
 		setDefaultCloseOperation(javax.swing.JFrame.DO_NOTHING_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout(0,0));
-		
-		setSize(575,310);		//Default Size for Main Window
-		
-		setVisible(false);
-		getContentPane().add(BorderLayout.CENTER, mainPanel);
-		mainPanel = new JPanel();
 		mainPanel.setLayout(null);
 		
+		setSize(575,310);		//Default Size for Main Window
+		setVisible(false);
+		
+		getContentPane().add(BorderLayout.CENTER, mainPanel);
+		model = getModel();
+		
+		mainPanel.add(scrollPane);
+		
+		for (JButton button: getButtons())
+			mainPanel.add(button);
+		
+		
 	}
-	
-	public List<JButton> getButtonList(){
-		return null;
-	}
+		
 	/*
 	 * (non-Javadoc)
 	 * @see java.awt.Window#setSize(int, int)
@@ -44,7 +54,11 @@ public class AbstractFrm extends JFrame{
 		mainPanel.setBounds(0, 0, wiegth, height);
 		super.setSize(wiegth, height);
 	}
+	public abstract List<JButton> getButtons();
+	public abstract DefaultTableModel getModel();
 	
-	private JPanel mainPanel;
+	private JPanel mainPanel= new JPanel();
+	private JScrollPane scrollPane = new JScrollPane();
+	private JTable table = new JTable();
 	
 }

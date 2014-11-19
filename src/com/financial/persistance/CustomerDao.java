672233735153e974c561d8caf85e9891a8af15db
rachana <rachana@rachana-XPS-L412Z>
2014-11-18 23:27:34 -0600

    Updated CustomerDao

--- src/com/financial/persistance/CustomerDao.java
+++ src/com/financial/persistance/CustomerDao.java
@@ -1,4 +1,25 @@
 package com.financial.persistance;
 
-public class CustomerDao{
+import com.financial.customers.ICustomer;
+
+public class CustomerDao<R> implements IDao<ICustomer, R>{
+
+	@Override
+	public boolean persist(ICustomer object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(ICustomer object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ICustomer getAccount(R object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }

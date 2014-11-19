ec07a07cd6ffddc96736f5d612b03f81b2877b79
rkkarn32 <rk.karn32@gmail.com>
2014-11-19 11:40:10 -0600

    Email feature added to ccard

--- src/com/ccard/Gold.java
+++ src/com/ccard/Gold.java
@@ -1,6 +1,9 @@
 package com.ccard;
 
 import com.financial.account.Entry;
+import com.financial.customers.Company;
+import com.financial.customers.Person;
+import com.financial.utilities.CommonResources;
 
 public class Gold extends CreditCardAccount {
 	
@@ -13,7 +16,20 @@ public class Gold extends CreditCardAccount {
 
 	@Override
 	public String hasToSendMail(Entry entry) {
-		// TODO Auto-generated method stub
+		if (this.getAccountHolder() instanceof Company) {
+			super.sendEmail(entry, "Transaction Done");
+			return "Transaction Done";
+		} else if (this.getAccountHolder() instanceof Person) {
+			if (this.getBalance() < 0) {
+				super.sendEmail(entry, "Balance is negative");
+				return "Balance is negative";
+			} 
+			if (entry.getEntryInfo().equals(CommonResources.TEXT_WITHDRAW)
+					&& entry.getAmount() > 400) {
+				super.sendEmail(entry, "Withdraw is larger than $400");
+				return "Withdraw is larger than $400";
+			}
+		}
 		return null;
 	}
 }
